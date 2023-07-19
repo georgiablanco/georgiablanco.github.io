1ba38ebc9c6352041bd98bb11218d2926568a8ea
georgiablanco <georgia.litchfield@hotmail.co.uk>
2023-07-19 10:21:54 +0100

    updating tabs and adding about me

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-// Wait for the document to load before running the script 
-(function ($) {
-  
-    // We use some Javascript and the URL #fragment to hide/show different parts of the page
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-    $(window).on('load hashchange', function(){
-      
-      // First hide all content regions, then show the content-region specified in the URL hash 
-      // (or if no hash URL is found, default to first menu item)
-      $('.content-region').hide();
-      
-      // Remove any active classes on the main-menu
-      $('.main-menu a').removeClass('active');
-      var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-      
-      // Now show the region specified in the URL hash
-      $(region).show();
-      
-      // Highlight the menu link associated with this region by adding the .active CSS class
-      $('.main-menu a[href="'+ region +'"]').addClass('active'); 
-  
-      // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-      // This will extract the region name from URL hash, and then load [region].html into the main #content div
-      // var region = location.hash.toString() || '#first';
-      // $('#content').load(region.slice(1) + '.html')
-      
-    });
-    
-  })(jQuery);
+// Generic function for initializing more than one menu in html
+function initializeMenu($, window, menuClass) {
+  function showContent(region) {
+    // First hide all content regions
+    $('.' + menuClass + '-region').hide();
+
+    // Remove any active classes on the menu items
+    $('.' + menuClass + '-menu a').removeClass('active');
+
+    // Now show the selected region
+    $(region).show();
+
+    // Highlight the menu link associated with this region by adding the .active CSS class
+    $('.' + menuClass + '-menu a[href="'+ region +'"]').addClass('active'); 
+  }
+
+  // Wait for the document to load before running the script
+  $(window).on('load hashchange', function() {
+    // Get the region specified in the URL hash (or default to the first menu item)
+    var region = location.hash.toString() || $('.' + menuClass + '-menu a:first').attr('href');
+    showContent(region);
+  });
+
+  // Handle click events for the specific menu
+  $('.' + menuClass + '-menu a').on('click', function(event) {
+    event.preventDefault();
+    var region = $(this).attr('href');
+    showContent(region);
+  });
+}
+
+// Usage
+(function($) {
+  var menuClasses = ['main', 'projects'];
+
+  for (var i = 0; i < menuClasses.length; i++) {
+    var menuClass = menuClasses[i];
+    initializeMenu($, window, menuClass);
+  }
+})(jQuery);
